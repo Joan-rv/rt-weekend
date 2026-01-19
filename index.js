d5cffe297593624f222aa2c66e3627dc35a55813
Joan Ripoll <Joan-rv@users.noreply.github.com>
2026-01-19 23:00:55 +0100

    Explicit worker initialization message

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let image = ctx.createImageData(canvas.width, canvas.height);
-let status = "stopped";
+let renderStatus = "stopped";
 
 function draw() {
-  if (status === "running") requestAnimationFrame(draw);
+  if (renderStatus === "running") requestAnimationFrame(draw);
   ctx.putImageData(image, 0, 0);
 }
 
@@ -16,9 +16,10 @@ function render() {
   const batchSize = 10;
   for (let i = 0; i < navigator.hardwareConcurrency; ++i) {
     const worker = new Worker("worker.js", { type: "module" });
+    worker.postMessage({ width: canvas.width, height: canvas.height });
     worker.onmessage = (e) => {
       if (e.data) {
-        for (let {
+        for (const {
           coords: { x, y },
           rgb: { r, g, b },
         } of e.data) {
@@ -28,14 +29,14 @@ function render() {
           image.data[4 * (y * canvas.width + x) + 3] = 0xff;
         }
       }
-      if (status === "running") {
+      if (renderStatus === "running") {
         worker.postMessage([
           renderPos,
           Math.min(renderPos + batchSize, canvas.width * canvas.height),
         ]);
         renderPos += batchSize;
         if (renderPos >= canvas.width * canvas.height) {
-          status = "stopped";
+          renderStatus = "stopped";
         }
       } else {
         worker.terminate();
@@ -46,11 +47,11 @@ function render() {
 
 function buttonPress() {
   const button = document.getElementById("button");
-  if (status === "running") {
-    status = "stopped";
+  if (renderStatus === "running") {
+    renderStatus = "stopped";
     button.innerHTML = "Render";
   } else {
-    status = "running";
+    renderStatus = "running";
     button.innerHTML = "Stop";
     render();
   }
--- worker.js
+++ worker.js
@@ -22,80 +22,90 @@ function randomVec3Range(min, max) {
   );
 }
 
-async function run() {
-  await init();
+let scene;
+let imageWidth;
+const initPromise = init();
 
-  seed = 123456789;
+onmessage = async (e) => {
+  await initPromise;
+  const data = e.data;
+  if (data.width && data.height) {
+    imageWidth = data.width;
+    const aspectRatio = data.width / data.height;
 
-  const imageWidth = 800;
-  const aspectRatio = 1.0;
+    seed = 123456789;
 
-  const hittables = [];
+    const hittables = [];
 
-  const checkerTexture = SceneBuilder.createTextureChecker(
-    0.32,
-    SceneBuilder.createTextureSolidColor(new Vec3(0.2, 0.3, 0.1)),
-    SceneBuilder.createTextureSolidColor(new Vec3(0.9, 0.9, 0.9)),
-  );
-  const groundMat = SceneBuilder.createMaterialLambertian(checkerTexture);
-  hittables.push(
-    SceneBuilder.createSphere(new Vec3(0.0, -1000.0, 0.0), 1000.0, groundMat),
-  );
+    const checkerTexture = SceneBuilder.createTextureChecker(
+      0.32,
+      SceneBuilder.createTextureSolidColor(new Vec3(0.2, 0.3, 0.1)),
+      SceneBuilder.createTextureSolidColor(new Vec3(0.9, 0.9, 0.9)),
+    );
+    const groundMat = SceneBuilder.createMaterialLambertian(checkerTexture);
+    hittables.push(
+      SceneBuilder.createSphere(new Vec3(0.0, -1000.0, 0.0), 1000.0, groundMat),
+    );
 
-  for (let a = -11; a < 11; a++) {
-    for (let b = -11; b < 11; b++) {
-      const choose_mat = random();
-      const center = new Vec3(a + 0.9 * random(), 0.2, b + 0.9 * random());
+    for (let a = -11; a < 11; a++) {
+      for (let b = -11; b < 11; b++) {
+        const choose_mat = random();
+        const center = new Vec3(a + 0.9 * random(), 0.2, b + 0.9 * random());
 
-      if (center.sub(new Vec3(4.0, 0.2, 0.0)).length() > 0.9) {
-        if (choose_mat < 0.8) {
-          const albedo = randomVec3().mulVec(randomVec3());
-          const sphereMat = SceneBuilder.createMaterialLambertian(
-            SceneBuilder.createTextureSolidColor(albedo),
-          );
-          hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
-        } else if (choose_mat < 0.95) {
-          const albedo = randomVec3Range(0.5, 1.0);
-          const fuzz = randomRange(0.0, 0.5);
-          const sphereMat = SceneBuilder.createMaterialMetal(albedo, fuzz);
-          hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
-        } else {
-          const sphereMat = SceneBuilder.createMaterialDielectric(1.5);
-          hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
+        if (center.sub(new Vec3(4.0, 0.2, 0.0)).length() > 0.9) {
+          if (choose_mat < 0.8) {
+            const albedo = randomVec3().mulVec(randomVec3());
+            const sphereMat = SceneBuilder.createMaterialLambertian(
+              SceneBuilder.createTextureSolidColor(albedo),
+            );
+            hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
+          } else if (choose_mat < 0.95) {
+            const albedo = randomVec3Range(0.5, 1.0);
+            const fuzz = randomRange(0.0, 0.5);
+            const sphereMat = SceneBuilder.createMaterialMetal(albedo, fuzz);
+            hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
+          } else {
+            const sphereMat = SceneBuilder.createMaterialDielectric(1.5);
+            hittables.push(SceneBuilder.createSphere(center, 0.2, sphereMat));
+          }
         }
       }
     }
-  }
 
-  const mat1 = SceneBuilder.createMaterialDielectric(1.5);
-  hittables.push(SceneBuilder.createSphere(new Vec3(0.0, 1.0, 0.0), 1.0, mat1));
+    const mat1 = SceneBuilder.createMaterialDielectric(1.5);
+    hittables.push(
+      SceneBuilder.createSphere(new Vec3(0.0, 1.0, 0.0), 1.0, mat1),
+    );
 
-  const mat2 = SceneBuilder.createMaterialLambertian(
-    SceneBuilder.createTextureSolidColor(new Vec3(0.4, 0.2, 0.1)),
-  );
-  hittables.push(
-    SceneBuilder.createSphere(new Vec3(-4.0, 1.0, 0.0), 1.0, mat2),
-  );
+    const mat2 = SceneBuilder.createMaterialLambertian(
+      SceneBuilder.createTextureSolidColor(new Vec3(0.4, 0.2, 0.1)),
+    );
+    hittables.push(
+      SceneBuilder.createSphere(new Vec3(-4.0, 1.0, 0.0), 1.0, mat2),
+    );
 
-  const mat3 = SceneBuilder.createMaterialMetal(new Vec3(0.7, 0.6, 0.5), 0.0);
-  hittables.push(SceneBuilder.createSphere(new Vec3(4.0, 1.0, 0.0), 1.0, mat3));
+    const mat3 = SceneBuilder.createMaterialMetal(new Vec3(0.7, 0.6, 0.5), 0.0);
+    hittables.push(
+      SceneBuilder.createSphere(new Vec3(4.0, 1.0, 0.0), 1.0, mat3),
+    );
 
-  const cameraDesc = {
-    imageWidth,
-    aspectRatio,
-    lookFrom: { x: 13.0, y: 2.0, z: 3.0 },
-    lookAt: { x: 0.0, y: 0.0, z: 0.0 },
-    defocusAngle: (0.6 * Math.PI) / 180.0,
-    focusDist: 10.0,
-    fovy: (20.0 * Math.PI) / 180.0,
-    samplesPerPx: 500,
-    maxDepth: 50,
-  };
-  const scene = SceneBuilder.build(cameraDesc, hittables);
+    const cameraDesc = {
+      imageWidth,
+      aspectRatio,
+      lookFrom: { x: 13.0, y: 2.0, z: 3.0 },
+      lookAt: { x: 0.0, y: 0.0, z: 0.0 },
+      defocusAngle: (0.6 * Math.PI) / 180.0,
+      focusDist: 10.0,
+      fovy: (20.0 * Math.PI) / 180.0,
+      samplesPerPx: 500,
+      maxDepth: 50,
+    };
+    scene = SceneBuilder.build(cameraDesc, hittables);
 
-  onmessage = (e) => {
+    postMessage(null);
+  } else {
     let pixels = [];
-    const [start, end] = e.data;
+    const [start, end] = data;
     for (let i = start; i < end; ++i) {
       const x = i % imageWidth;
       const y = Math.floor(i / imageWidth);
@@ -105,9 +115,5 @@ async function run() {
       pixels.push({ coords: { x, y }, rgb: { r, g, b } });
     }
     postMessage(pixels);
-  };
-
-  postMessage(null);
-}
-
-run();
+  }
+};
